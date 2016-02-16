9219e6c41706e527d4cc6f56a140526e4aeaf4c7
skiloski1 <skiloski1@gmail.com>
2016-02-16 12:49:02 +0100

    Added OddNumbers

---
+++ exercises/odd_numbers.py
@@ -0,0 +1,15 @@
+def even_numbers(low, high):
+	if low<high:
+		if type(low) != int and type(high) != int:
+			return "Arguments are not strings"
+			
+		num = range(low, high)
+		mylist = []
+		for i in num:
+			if i%2!=0:
+				mylist.append(i)
+		return mylist
+	else:
+		return "Error, Arguments are incorrect"
+
+print(even_numbers(10, "Hello"));
