a182561039674592e6ab4d66ee21c25876b23175
skiloski1 <skiloski1@gmail.com>
2016-02-17 23:06:11 +0100

    added prime.py file

--- exercises/prime.py
+++ exercises/prime.py
@@ -8,19 +8,10 @@ def is_prime(num):
             return False;
     return True;
 
-def find(n):
-	i = 2;
-	while i*i < n:
-		while n%i==0:
-			n = n/i
-		i = i+1
-	print(n);
-
-def find2(num):
-	result = 0
-	for i in range(2, 999999):
-		if is_prime(i) and num%i==0 and result<i:
-			result = i
-	return result
-
-print(find(120))
+def get_largest_prime(num):
+	for i in range(2, int(sqrt(num))+1):
+		while num%i==0:
+			if is_prime(num):
+				break;
+			num = num/i
+	return num
