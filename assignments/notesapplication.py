89c99a5f1dc3511e5d7ea0c949e5e709299fbafe
skiloski1 <skiloski1@gmail.com>
2016-02-26 22:07:07 +0100

    Updated Assignment

--- assignments/notesapplication.py
+++ assignments/notesapplication.py
@@ -1,4 +1,12 @@
 class NotesApplication(object):
+	"""
+		Base Class for Creating NotesApplication Objects
+
+		Args:
+			author(string): The First Parameter of the NotesApplication Class specifying the name of the Author
+			notes(optional[list]): The Second optional Parameter specifying a pre-exisiting note
+
+	"""
 	def __init__(self, author, notes=[]):
 		self.author = author
 		if type(notes)==str and notes!='':
@@ -10,12 +18,24 @@ class NotesApplication(object):
 
 
 	def create(self, note_content):
+		"""
+			Method creates a new Note for a NotesApplication Object
+
+			Args:
+				note_content(string): Specifies the note to create
+		"""
 		if len(note_content)>0:
 
 			self.notes.append(note_content)
 
 
 	def list(self):
+		"""
+			Method lists all notes in a NotesApplication Object
+
+			Returns:
+				list: List of All notes
+		"""
 		if len(self.notes)>0:
 
 			for k,v in enumerate(self.notes):
@@ -27,6 +47,15 @@ class NotesApplication(object):
 		return self.notes
 
 	def get(self, note_id):
+		"""
+			Method retrieves a specific note in a NotesApplication Object
+
+			Args:
+				note_id(int): Specifies the index of the note to retrieve
+
+			Returns:
+				str: The specific note retrieved
+		"""
 		index_length = len(self.notes) -1
 		if note_id<0 or note_id > index_length:
 			return "Invalid Index"
@@ -34,6 +63,16 @@ class NotesApplication(object):
 			return self.notes[note_id]
 
 	def search(self, search_text):
+
+		"""
+			Method searches for a specific string in the NotesApplication Object
+
+			Args:
+				search_text(str): Specifies search string
+
+			Returns:
+				list: List of notes matching the search text
+		"""
 		
 		search_notes = filter(lambda x: search_text in x, self.notes)
 		if len(search_notes)==0:
@@ -48,6 +87,15 @@ class NotesApplication(object):
 			return search_notes
 
 	def delete(self, note_id):
+		"""
+			Method deletes a specific note in the NotesApplication Object
+
+			Args:
+				note_id(int): Specifies the Id of the note to delete
+
+			Returns:
+				bool: Returns True when Id is Invalid
+		"""
 		note_length = len(self.notes) -1
 		if note_id<0 or note_id > note_length:
 			print "\nInvalid Index"
@@ -58,6 +106,16 @@ class NotesApplication(object):
 			print("\n Note with index %d has been successfully deleted! Taking you back to main menu" % note_id)
 
 	def edit(self, note_id, new_content):
+		"""
+			Method deletes a specific note in the NotesApplication Object
+
+			Args:
+				note_id(int): Specifies the Id of the note to delete
+				new_content(str): The new string to replace in the NotesApplication Object
+
+			Returns:
+				bool: Returns True when Id is Invalid
+		"""
 		note_length = len(self.notes) -1
 		if note_id<0 or note_id > note_length:
 			print "\nInvalid Index"
